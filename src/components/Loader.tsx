c697bbbf14b692a3fb3399814e51d1051654eba8
Rakshit Kumar <rkstlohchab@gmail.com>
2024-09-20 00:37:35 +0530

    logo changes

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -5,7 +5,7 @@ import styles from "./Loader.module.css";
 export default function Loader() {
   return (
     <div className="z-10 flex flex-col items-center justify-center w-screen h-screen gap-2">
-      <Image src="/Atom2.jpg" width={100} height={100} alt="logo" />
+      <Image src="https://drive.google.com/uc?export=view&id=1xe8Vbb-oMjCJfGGzH3Nf_P4TzCnxXUNY" width={100} height={100} alt="logo" />
       <div className={styles.container}></div>
     </div>
   )
