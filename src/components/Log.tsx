cc811dd14758eb69e3741f031071759db9ba02ce
Rakshit Kumar <rkstlohchab@gmail.com>
2024-09-25 18:20:33 +0530

    fix

--- src/components/Log.tsx
+++ src/components/Log.tsx
@@ -33,19 +33,23 @@ export default function Log() {
 
   return (
     <div className="lg:px-[120px] lg:py-[30px] max-sm:px-10 max-sm:py-5">
+      <h1 className="w-full text-start text-2xl my-2">Blogs</h1>
+
       {blogList?.map((blog) => (
         <Card
             key={blog._id}
             className="m-0 p-0 border-none shadow-none inline-block cursor-pointer"
             onClick={() => handleCardClick(blog._id)}
         >
-            <CardHeader className=" lg:mr-10 lg:my-10 max-sm:mt-8">
+            <CardHeader className="gap-2 lg:mr-10 lg:my-10 max-sm:mt-8">
                 <Image
                     src={blog.image}
                     alt={blog.title}
-                    quality={70}
-                    width={300} height={210} className="w-[360px] max-lg:h-[170px] lg:h-[270px]"
+                    quality={40}
+                    width={350} height={210} className="w-[350px] max-lg:h-[170px] lg:h-[210px]"
                 />
+                <CardTitle className="font-normal ">{blog.title}</CardTitle>
+                <CardDescription className="">{blog.description.split(' ').slice(0, 3).join(' ')}</CardDescription>
             </CardHeader>
         </Card>
 
