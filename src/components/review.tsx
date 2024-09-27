979b1183b645cb3eb358528f19019bde37304562
Rakshit Kumar <rkstlohchab@gmail.com>
2024-09-27 19:48:50 +0530

    carousel fix

--- src/components/review.tsx
+++ src/components/review.tsx
@@ -8,52 +8,63 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 
-import { Card, CardContent } from "@/components/ui/card"
 import Autoplay from "embla-carousel-autoplay"
+import { Button } from "./ui/button"
 
 
 const Review = () => {
   return (
-    <div className="lg:mb-15 flex max-lg:flex-col flex-row w-full items-center bg-[#2f4462]">
-      <div className="w-1/2">
-        <h2 className="text-5xl text-center my-5 lg:my-10 text-white">
-          WHAT OUR <br/>CLIENTS SAY
-        </h2>
+    <div className="lg:mb-15 flex flex-col w-screen h-screen items-center bg-[#e8e8e8]">
+
+      <h2 className="text-lg text-center my-5 lg:my-10 text-black w-full ">
+        T E S T I M O N I A L S
+      </h2>
+
+      <div className="m-10 ">
+        <Carousel
+          plugins={[
+                Autoplay({
+                delay: 2000,
+                }),
+              ]}
+          >
+            <CarouselContent>
+              {testimonials.map((testimonial, index) => (
+                <CarouselItem key={index} className="lg:hidden">
+                  <div className="bg-white lg:hidden rounded-sm w-full h-[500px] flex flex-col items-center justify-around shadow-sm">
+                    <p className="text-justify max-lg:text-sm flex flex-wrap w-2/3">{testimonial.text}</p>
+                    <div className="">
+                      <h3 className="text-2xl font-bold">{testimonial.user}</h3>
+                      <p className="text-xxl font-bold">{testimonial.company}</p>
+                    </div>
+                  </div>
+                </CarouselItem>
+              ))}
+
+              {testimonials.map((testimonial, index) => (
+                <CarouselItem key={index} className="max-lg:hidden basis-1/3">
+                  <div className="bg-white max-lg:hidden rounded-sm w-[350px] h-[500px] flex flex-col items-center justify-around shadow-sm">
+                    <p className="text-justify max-lg:text-sm flex flex-wrap w-2/3">{testimonial.text}</p>
+                    <div className="">
+                      <h3 className="text-2xl font-bold">{testimonial.user}</h3>
+                      <p className="text-xxl font-bold">{testimonial.company}</p>
+                    </div>
+                  </div>
+                </CarouselItem>
+              ))}
+
+              
+            </CarouselContent>
+        </Carousel>
       </div>
 
-      {/* <Carousel 
-          className="flex flex-wrap items-center justify-center max-lg:w-full w-1/2 "
-           plugins={[
-              Autoplay({
-              delay: 2000,
-              }),
-          ]}
-      >
-          <CarouselContent>
-              {testimonials.map((testimonial, index) => (
-                  <CarouselItem key={index}>
-                    <div className="lg:px-6 lg:py-2">
-                      <Card>
-                          <CardContent className="flex flex-col aspect-square items-center justify-center p-6 ">
-                              <p className="text-justify max-lg:text-sm flex flex-wrap w-2/3">{testimonial.text}</p>
-                              <div className="flex mt-4">
-                                  <div>
-                                      <span className="text-sm font-normal italic text-neutral-600">
-                                              <h1 className="text-2xl font-bold">{testimonial.user}</h1>
-                                              {testimonial.company}
-                                      </span>
-                                  </div>
-                              </div>
-                          </CardContent>
-                      </Card>
-                    </div>
-                  </CarouselItem>
-                  
-              ))}
-          </CarouselContent>
-          <CarouselPrevious  className="max-md:hidden"/>
-          <CarouselNext className="max-md:hidden" />
-      </Carousel> */}
+
+      <ul className="flex text-[12px] items-center">
+        <li className="text-black hover:text-blue-400 active:text-blue-400">
+            <Button className="bg-orange-500"><a href="https://forms.gle/YDVrxun83nFkhJst6">CONTACT US</a></Button>
+        </li>
+      </ul>
+
 
     </div>
 
---
+++ src/components/testimonialCard.tsx
@@ -0,0 +1,18 @@
+// TestimonialCard.js
+import React from 'react';
+
+const TestimonialCard = ({ testimonial }:any) => {
+  return (
+    <div className="bg-white flex flex-col items-center justify-between p-8 rounded-lg shadow-md h-[550px] w-[350px]">
+      <p className="text-justify max-lg:text-sm flex flex-wrap w-2/3">{testimonial.text}</p>
+      <div className="flex mt-4">
+        <span className="text-sm font-normal italic text-neutral-600">
+          <h1 className="text-2xl font-bold">{testimonial.user}</h1>
+          {testimonial.company}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default TestimonialCard;
