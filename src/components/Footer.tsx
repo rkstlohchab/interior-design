d5316b05855272e7e5f94ca45a92ecbbbdc3172f
Rakshit Kumar <rkstlohchab@gmail.com>
2024-09-27 19:50:37 +0530

    carousel fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Header = () => {
     return (
-            <div className="max-lg:text-[10px] lg:text-[13px] flex flex-row justify-between items-center w-full h-[65px] lg:py-[15px] lg:px-[70px] max-sm:px-10 bg-white z-40 border-gray-300 border-t-2 fixed bottom-0">
+            <div className="max-lg:text-[10px] lg:text-[13px] flex flex-row justify-between items-center w-full h-[65px] lg:py-[15px] lg:px-[70px] max-sm:px-10 bg-white z-40 border-gray-300 border-t-2 sticky bottom-0">
                 <div className="w-1/2 flex justify-start flex-wrap flex-col">
                     <p>Pno:- 8950520252</p>
                     <p>Address:- Sec 67</p>
